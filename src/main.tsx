7955542b994dca4026807c5ffc59aab7d392e721
Ri <ri@3sidedcube.com>
2023-06-23 12:05:21 +0100

    Customising Query Settings

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,19 @@ import App from "./App";
 import "./index.css";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  // defaultOptions: {
+  //   queries: {
+  //     retry: 3, // default number of retries after a failed request
+  //     cacheTime: 300_000, // default 5mins; no observer (no component using the query) therefore inactive (removed from cache)
+  //     staleTime: 3 * 1000, // default 0, 3 seconds as an example here
+  //     // auto-refresh
+  //     refetchOnWindowFocus: true, // window refocused
+  //     refetchOnReconnect: true, // network reconnected
+  //     refetchOnMount: true, // component mounted
+  //   },
+  // },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
