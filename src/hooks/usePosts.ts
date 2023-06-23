81cee0a95218555ae18c3a702fb8932f74c98253
Ri <ri@3sidedcube.com>
2023-06-23 14:08:12 +0100

    Parameterized Queries (& Caching)

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -8,15 +8,25 @@ interface Post {
   userId: number;
 }
 
-const usePosts = () => {
+const usePosts = (userId: number | undefined) => {
   const getPosts = () => {
     return axios
-      .get<Post[]>("https://jsonplaceholder.typicode.com/posts")
+      .get<Post[]>(
+        // `https://jsonplaceholder.typicode.com/posts/?userId=${userId}` // ugly method
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+          params: {
+            userId,
+          },
+        }
+      )
       .then((res) => res.data);
   };
 
   return useQuery<Post[], Error>({
-    queryKey: ["posts"],
+    // users/1/posts
+    // when the userId changes, the query is re-executed bc it is a param like dependency in useEffect
+    queryKey: userId ? ["users", userId, "posts"] : ["posts"],
     queryFn: getPosts,
     staleTime: 1 * 60 * 1000, // 1 minute
   });
