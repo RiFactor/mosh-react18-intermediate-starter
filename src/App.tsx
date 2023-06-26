4543aef7addba87e9030ab2d142199c448121622
Ri <ri@3sidedcube.com>
2023-06-26 10:31:28 +0100

    Mutate Data

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import "./App.css";
 import PostList from "./react-query/PostList";
 import TodoList from "./react-query/TodoList";
 import InfinitePostList from "./react-query/InfinitePostList";
+import TodoForm from "./react-query/TodoForm";
 
 function App() {
   return (
     <div>
       <h1>React Starter Project</h1>
-      {/* <TodoList /> */}
-      <PostList />
+      <TodoForm />
+      <TodoList />
+      {/* <PostList /> */}
       {/* <InfinitePostList /> */}
     </div>
   );
--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,15 +1,48 @@
-import { useRef } from 'react';
+import { useRef } from "react";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { Todo } from "../hooks/useTodos";
 
 const TodoForm = () => {
+  const addTodo = useMutation({
+    mutationFn: (todo: Todo) => {
+      // Question -- do I want this to be an async fn?
+      return axios
+        .post<Todo>("https://jsonplaceholder.typicode.com/posts", todo)
+        .then((res) => res.data);
+    },
+    // onSucess, onError (toast notification), onSettled (either outcome)
+    onSuccess: (savedTodo, newTodo) => console.log(savedTodo),
+  });
+
   const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <form className="row mb-3">
+    <form
+      className="row mb-3"
+      onSubmit={(event) => {
+        event.preventDefault();
+
+        if (ref.current && ref.current.value) {
+          addTodo.mutate({
+            id: 0, // BED would do
+            title: ref.current?.value,
+            completed: false,
+            userId: 1, // usually dynamic
+          });
+        }
+      }}
+    >
       <div className="col">
         <input ref={ref} type="text" className="form-control" />
       </div>
       <div className="col">
-        <button className="btn btn-primary">Add</button>
+        <button
+          // disabled={!ref.current || !ref.current.value}
+          className="btn btn-primary"
+        >
+          Add
+        </button>
       </div>
     </form>
   );
