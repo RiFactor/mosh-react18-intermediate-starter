3e03c249a3361071ad247cfd39e19eb86e7a7a91
Ri <ri@3sidedcube.com>
2023-06-26 12:44:33 +0100

    Custom Mutation Hook

---
+++ src/hooks/useAddTodo.ts
@@ -0,0 +1,53 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { Todo } from "./useTodos";
+import { CACHE_KEY_TODOS } from "../react-query/constants";
+
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
+
+const useAddTodo = (addTodo: () => void) => {
+  const queryClient = useQueryClient();
+
+  // "variables" in <> are what you pass in
+  return useMutation<Todo, Error, Todo, AddTodoContext>({
+    mutationFn: (todo: Todo) => {
+      // Question -- do I want this to be an async fn?
+      return axios
+        .post<Todo>("https://jsonplaceholder.typicode.com/posts", todo)
+        .then((res) => res.data);
+    },
+    onMutate: (newTodo: Todo) => {
+      const previousTodos =
+        queryClient.getQueryData<Todo[]>(CACHE_KEY_TODOS) || [];
+      // APPROACH 2: update cache data
+      queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos) => [
+        newTodo,
+        ...(todos || []),
+      ]);
+
+      addTodo();
+
+      return { previousTodos };
+    },
+    // onSucess, onError (toast notification), onSettled (either outcome)
+    onSuccess: (savedTodo, newTodo) => {
+      // APPROACH 1: invalidate the Cache: won't work for JSON placeholder b/c a fake API
+      // queryClient.invalidateQueries({
+      //   queryKey: CACHE_KEY_TODOS
+      // });
+      queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos) =>
+        todos?.map((todo) => {
+          return todo === newTodo ? savedTodo : todo;
+        })
+      );
+    },
+    onError: (error, newTodo, context) => {
+      if (!context) return;
+      queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, context.previousTodos);
+    },
+  });
+};
+
+export default useAddTodo;
--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,55 +1,13 @@
 import { useRef } from "react";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import axios from "axios";
-import { Todo } from "../hooks/useTodos";
-
-interface AddTodoContext {
-  previousTodos: Todo[];
-}
+import useAddTodo from "../hooks/useAddTodo";
 
 const TodoForm = () => {
-  const queryClient = useQueryClient();
-
-  // "variables" in <> are what you pass in
-  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
-    mutationFn: (todo: Todo) => {
-      // Question -- do I want this to be an async fn?
-      return axios
-        .post<Todo>("https://jsonplaceholder.typicode.com/posts", todo)
-        .then((res) => res.data);
-    },
-    onMutate: (newTodo: Todo) => {
-      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
-      // APPROACH 2: update cache data
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-        newTodo,
-        ...(todos || []),
-      ]);
-      if (ref.current) {
-        ref.current.value = "";
-      }
-
-      return { previousTodos };
-    },
-    // onSucess, onError (toast notification), onSettled (either outcome)
-    onSuccess: (savedTodo, newTodo) => {
-      // APPROACH 1: invalidate the Cache: won't work for JSON placeholder b/c a fake API
-      // queryClient.invalidateQueries({
-      //   queryKey: ["todos"]
-      // });
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
-        todos?.map((todo) => {
-          return todo === newTodo ? savedTodo : todo;
-        })
-      );
-    },
-    onError: (error, newTodo, context) => {
-      if (!context) return;
-      queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos);
-    },
-  });
-
   const ref = useRef<HTMLInputElement>(null);
+  const addTodo = useAddTodo(() => {
+    if (ref.current) {
+      ref.current.value = "";
+    }
+  });
 
   return (
     <>
