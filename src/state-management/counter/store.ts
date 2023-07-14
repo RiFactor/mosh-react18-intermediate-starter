ffe9f6d479babff247f618be4e1c4e1bee44b5f7
Ri <ri@3sidedcube.com>
2023-07-14 15:02:08 +0100

    Prevent excess renders w/ selectors

--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -2,6 +2,7 @@ import { create } from "zustand";
 
 interface CounterStore {
   counter: number;
+  max: number;
   increment: () => void;
   reset: () => void;
 }
@@ -9,8 +10,9 @@ interface CounterStore {
 // not doing export default here b/c won't be named and easily found or imported
 const useCounterStore = create<CounterStore>((set) => ({
   counter: 0,
+  max: 5,
   increment: () => set((store) => ({ counter: store.counter + 1 })),
-  reset: () => set(() => ({ counter: 0 })),
+  reset: () => set(() => ({ max: 10 })),
 }));
 
 export default useCounterStore;
