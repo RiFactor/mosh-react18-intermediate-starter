5aeb404749f4217ec67fcec302643d4fbf679d5e
Ri <ri@3sidedcube.com>
2023-07-17 12:30:37 +0100

    Nested Routes

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -3,12 +3,20 @@ import HomePage from "./HomePage";
 import UserListPage from "./UserListPage";
 import ContactPage from "./ContactPage";
 import UserDetailPage from "./UserDetailPage";
+import Layout from "./Layout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/users", element: <UserListPage /> },
-  { path: "/contact", element: <ContactPage /> },
-  { path: "/users/:id", element: <UserDetailPage /> },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      // children's paths don't need '/' before but <Link to="/users" /> does
+      { index: true, element: <HomePage /> }, // path: "" // either work
+      { path: "users", element: <UserListPage /> },
+      { path: "contact", element: <ContactPage /> },
+      { path: "users/:id", element: <UserDetailPage /> },
+    ],
+  },
 ]);
 
 export default router;
